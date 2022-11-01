72fee1b7f210a96bbe7baa416ed90f18e9ad6f8b
ilya <rudakov.alhan@gmail.com>
2022-11-01 19:45:23 +0300

    feat: add solution task 4

--- unit_11/unit_11.js
+++ unit_11/unit_11.js
@@ -46,6 +46,24 @@ let delimiter4 = '-';
 
 const f4 = (arr, delimiter) => {
 
+    let str = '';
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (i === arr.length - 1) {
+
+            str += `${arr[i]}`
+
+        } else {
+
+            str += `${arr[i]}${delimiter}`
+
+        }
+
+    }
+
+    return str;
+
 }
 
 // TASK 05
